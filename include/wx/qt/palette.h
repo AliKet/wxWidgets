05bf4ee6043f98157cf2e6e7a8e587d3ab206fd2
ali kettab <aliket1435@gmail.com>
2023-10-10 20:38:51 +0100

    Use the generic implementation of wxPalette under wxQt

--- include/wx/qt/palette.h
+++ include/wx/qt/palette.h
@@ -8,16 +8,33 @@
 #ifndef _WX_QT_PALETTE_H_
 #define _WX_QT_PALETTE_H_
 
-class WXDLLIMPEXP_CORE wxPalette : public wxPaletteBase
+#include "wx/defs.h"
+#include "wx/object.h"
+#include "wx/gdiobj.h"
+#include "wx/gdicmn.h"
+
+//-----------------------------------------------------------------------------
+// classes
+//-----------------------------------------------------------------------------
+
+class WXDLLIMPEXP_FWD_CORE wxPalette;
+
+//-----------------------------------------------------------------------------
+// wxPalette
+//-----------------------------------------------------------------------------
+
+class WXDLLIMPEXP_CORE wxPalette: public wxPaletteBase
 {
 public:
     wxPalette();
-    wxPalette(int n, unsigned char *red, unsigned char *green, unsigned char *blue);
+    wxPalette( int n, const unsigned char *red, const unsigned char *green, const unsigned char *blue );
+    virtual ~wxPalette();
 
-    bool Create(int n, unsigned char *red, unsigned char *green, unsigned char *blue);
+    bool Create( int n, const unsigned char *red, const unsigned char *green, const unsigned char *blue);
+    int GetPixel( unsigned char red, unsigned char green, unsigned char blue ) const;
+    bool GetRGB( int pixel, unsigned char *red, unsigned char *green, unsigned char *blue ) const;
 
-    bool GetRGB(int pixel, unsigned char *red, unsigned char *green, unsigned char *blue) const;
-    int GetPixel(unsigned char red, unsigned char green, unsigned char blue) const;
+    virtual int GetColoursCount() const override;
 
 protected:
     virtual wxGDIRefData *CreateGDIRefData() const override;
@@ -25,7 +42,6 @@ protected:
 
 private:
     wxDECLARE_DYNAMIC_CLASS(wxPalette);
-
 };
 
 #endif // _WX_QT_PALETTE_H_
--- src/qt/palette.cpp
+++ src/qt/palette.cpp
@@ -1,48 +1,148 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        src/qt/palette.cpp
-// Author:      Peter Most
-// Copyright:   (c) Peter Most
+// Name:        src/generic/paletteg.cpp
+// Purpose:
+// Author:      Robert Roebling
+// Created:     01/02/97
+// Copyright:   (c) 1998 Robert Roebling and Julian Smart
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
 
 // For compilers that support precompilation, includes "wx.h".
 #include "wx/wxprec.h"
 
+
+#if wxUSE_PALETTE
+
 #include "wx/palette.h"
 
-wxIMPLEMENT_DYNAMIC_CLASS(wxPalette,wxGDIObject)
+#include <vector>
+
+//-----------------------------------------------------------------------------
+// wxPalette
+//-----------------------------------------------------------------------------
+
+struct wxPaletteEntry
+{
+    unsigned char red, green, blue;
+};
+
+struct wxPaletteRefData : public wxGDIRefData
+{
+    wxPaletteRefData() = default;
+    wxPaletteRefData(const wxPaletteRefData& other);
+
+    std::vector<wxPaletteEntry> m_entries;
+};
+
+// We need to define the copy ctor explicitly because the base class copy
+// ctor is deleted.
+wxPaletteRefData::wxPaletteRefData(const wxPaletteRefData& palette)
+    : wxGDIRefData()
+{
+    m_entries = palette.m_entries;
+}
+
+//-----------------------------------------------------------------------------
+
+#define M_PALETTEDATA ((wxPaletteRefData *)m_refData)
+
+wxIMPLEMENT_DYNAMIC_CLASS(wxPalette,wxGDIObject);
 
 wxPalette::wxPalette()
 {
+    m_refData = nullptr;
 }
 
-wxPalette::wxPalette(int WXUNUSED(n), unsigned char *WXUNUSED(red), unsigned char *WXUNUSED(green), unsigned char *WXUNUSED(blue))
+wxPalette::wxPalette(int n, const unsigned char *red, const unsigned char *green, const unsigned char *blue)
+{
+    Create(n, red, green, blue);
+}
+
+wxPalette::~wxPalette()
 {
 }
 
-bool wxPalette::Create(int WXUNUSED(n), unsigned char *WXUNUSED(red), unsigned char *WXUNUSED(green), unsigned char *WXUNUSED(blue))
+int wxPalette::GetColoursCount() const
 {
-    return false;
+    if (m_refData)
+        return M_PALETTEDATA->m_entries.size();
+
+    return 0;
 }
 
-bool wxPalette::GetRGB(int WXUNUSED(pixel), unsigned char *WXUNUSED(red), unsigned char *WXUNUSED(green), unsigned char *WXUNUSED(blue)) const
+bool wxPalette::Create(int n,
+                       const unsigned char *red,
+                       const unsigned char *green,
+                       const unsigned char *blue)
 {
-    return false;
+    UnRef();
+    m_refData = new wxPaletteRefData();
+
+    M_PALETTEDATA->m_entries.resize(n);
+
+    int i = 0;
+    for ( wxPaletteEntry& e : M_PALETTEDATA->m_entries )
+    {
+        e.red = red[i];
+        e.green = green[i];
+        e.blue = blue[i];
+
+        ++i;
+    }
+
+    return true;
 }
 
-int wxPalette::GetPixel(unsigned char WXUNUSED(red), unsigned char WXUNUSED(green), unsigned char WXUNUSED(blue)) const
+int wxPalette::GetPixel( unsigned char red,
+                         unsigned char green,
+                         unsigned char blue ) const
 {
-    return false;
+    if (!m_refData) return wxNOT_FOUND;
+
+    int closest = 0;
+    double d,distance = 1000.0; // max. dist is 256
+
+    int i = 0;
+    for ( const wxPaletteEntry& e : M_PALETTEDATA->m_entries )
+    {
+        if ((d = 0.299 * abs(red - e.red) +
+                 0.587 * abs(green - e.green) +
+                 0.114 * abs(blue - e.blue)) < distance) {
+            distance = d;
+            closest = i;
+        }
+
+        ++i;
+    }
+    return closest;
 }
 
+bool wxPalette::GetRGB(int pixel,
+                       unsigned char *red,
+                       unsigned char *green,
+                       unsigned char *blue) const
+{
+    if ( !m_refData )
+        return false;
+
+    if ( pixel < 0 || (unsigned)pixel >= M_PALETTEDATA->m_entries.size() )
+        return false;
+
+    wxPaletteEntry& p = M_PALETTEDATA->m_entries[pixel];
+    if (red) *red = p.red;
+    if (green) *green = p.green;
+    if (blue) *blue = p.blue;
+    return true;
+}
 
 wxGDIRefData *wxPalette::CreateGDIRefData() const
 {
-    return nullptr;
+    return new wxPaletteRefData;
 }
 
-wxGDIRefData *wxPalette::CloneGDIRefData(const wxGDIRefData *WXUNUSED(data)) const
+wxGDIRefData *wxPalette::CloneGDIRefData(const wxGDIRefData *data) const
 {
-    return nullptr;
+    return new wxPaletteRefData(*static_cast<const wxPaletteRefData *>(data));
 }
 
+#endif // wxUSE_PALETTE
