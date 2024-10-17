3fe973e72d63b2f7c2fd3133d09217a58aa282bf
AliKet <AliKet@users.noreply.github.com>
2024-10-17 14:03:51 +0100

    Update enterleave.cpp

--- tests/events/enterleave.cpp
+++ tests/events/enterleave.cpp
@@ -43,65 +43,92 @@ TEST_CASE("EnterLeaveEvents", "[wxEvent][enter-leave]")
     EventCounter enter(panel.get(), wxEVT_ENTER_WINDOW);
     EventCounter leave(panel.get(), wxEVT_LEAVE_WINDOW);
 
-    // Wait for the first paint event to be sure
-    // that window really has its final size.
+    // Wait for the first paint event to be sure that window really
+    // has its final size.
     WaitForPaint waitForPaint(panel.get());
     panel->Show();
     waitForPaint.YieldUntilPainted();
 
     wxUIActionSimulator sim;
 
-    sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
-    wxYield();
+    SECTION("Without mouse capture")
+    {
+        sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
 
-    CHECK( enter.GetCount() == 1 );
-    CHECK( leave.GetCount() == 0 );
+        CHECK( enter.GetCount() == 1 );
+        CHECK( leave.GetCount() == 0 );
 
-    enter.Clear();
+        enter.Clear();
 
-    sim.MouseMove(button->GetScreenPosition() + wxPoint(10, 5));
-    wxYield();
+        sim.MouseMove(button->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
 
-    CHECK( enter.GetCount() == 0 );
-    CHECK( leave.GetCount() == 1 );
+        // The parent window (panel) should receive wxEVT_LEAVE_WINDOW event
+        // when mouse enters the child window (button) 
+        CHECK( enter.GetCount() == 0 );
+        CHECK( leave.GetCount() == 1 );
 
-    leave.Clear();
+        leave.Clear();
 
-    EventCounter clicked(button, wxEVT_LEFT_DOWN);
+        sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
 
-    sim.MouseDown();
-//    wxYield();
-    // At least under wxMSW calling wxYield() just once doesn't always work, so
-    // try for a while.
-    WaitFor("button to be clicked", [&]() { return clicked.GetCount() != 0; });
+        // Now it (panel) should receive wxEVT_ENTER_WINDOW event when
+        // the mouse leaves the button and enters the panel again.
+        CHECK( enter.GetCount() == 1 );
+        CHECK( leave.GetCount() == 0 );
 
-    sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
-    wxYield();
+        enter.Clear();
+    }
 
-    CHECK( enter.GetCount() == 0 );
-    CHECK( leave.GetCount() == 0 );
+    SECTION("With (implicit) mouse capture")
+    {
+        sim.MouseMove(button->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
 
-    sim.MouseMove(textctrl->GetScreenPosition() + wxPoint(10, 5));
-    wxYield();
+        enter.Clear();
+        leave.Clear();
 
-    CHECK( enter.GetCount() == 0 );
-    CHECK( leave.GetCount() == 0 );
+        sim.MouseDown();
+        wxYield();
 
-    sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
-    wxYield();
+        // Holding the mouse button down (initiated on the button) and then
+        // hovering over the panel should not generate any events (enter/leave)
+        // Additionally, entering and leaving another child (textctrl) while the
+        // mouse is still held down should also not generate any events.
 
-    CHECK( enter.GetCount() == 0 );
-    CHECK( leave.GetCount() == 0 );
+        sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
 
-    sim.MouseUp();
-    wxYield();
+        CHECK( enter.GetCount() == 0 );
+        CHECK( leave.GetCount() == 0 );
 
-#ifndef __WXGTK__
-    CHECK( enter.GetCount() == 1 );
-#else
-    CHECK( enter.GetCount() == 0 );
-#endif
-    CHECK( leave.GetCount() == 0 );
+        sim.MouseMove(textctrl->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
+
+        CHECK( enter.GetCount() == 0 );
+        CHECK( leave.GetCount() == 0 );
+
+        sim.MouseMove(panel->GetScreenPosition() + wxPoint(5, 5));
+        wxYield();
+
+        CHECK( enter.GetCount() == 0 );
+        CHECK( leave.GetCount() == 0 );
+
+        sim.MouseUp();
+        wxYield();
+
+        // wxGTK behaves differently here, as it does not generate a
+        // wxEVT_ENTER_WINDOW event when we release the mouse button.
+
+    #ifndef __WXGTK__
+        CHECK( enter.GetCount() == 1 );
+    #else
+        CHECK( enter.GetCount() == 0 );
+    #endif
+        CHECK( leave.GetCount() == 0 );
+    }
 }
 
 #endif // wxUSE_UIACTIONSIMULATOR
