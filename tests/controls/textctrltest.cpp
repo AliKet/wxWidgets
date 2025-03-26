f1d1fe904f6bbdbb2c995586f08ff7a1def65642
AliKet <aliket.83@gmail.com>
2025-03-27 00:19:02 +0100

    Enable TextCtrlTestCase::LongText() test to run under all platforms
    SetMaxLength() is removed and will be tested thoroughly in the upcoming commit.

--- tests/controls/textctrltest.cpp
+++ tests/controls/textctrltest.cpp
@@ -679,9 +679,6 @@ void TextCtrlTestCase::LogTextCtrl()
 
 void TextCtrlTestCase::LongText()
 {
-    // In the other ports SetMaxLength() can't be used with multi line text
-    // controls.
-#if defined(__WXMSW__) || defined(__WXGTK__)
     delete m_text;
     CreateText(wxTE_MULTILINE|wxTE_DONTWRAP);
 
@@ -692,7 +689,6 @@ void TextCtrlTestCase::LongText()
     const wxString linePattern = MakeLinePattern();
 
     // Fill the control.
-    m_text->SetMaxLength(15000);
     for (i = 0; i < numLines; i++)
     {
         m_text->AppendText(wxString::Format(wxT("[%3d] %s\n"), i, linePattern));
@@ -705,7 +701,6 @@ void TextCtrlTestCase::LongText()
         wxString line = m_text->GetLineText(i);
         CPPUNIT_ASSERT_EQUAL( line, pattern );
     }
-#endif // __WXMSW__
 }
 
 void TextCtrlTestCase::PositionToCoords()
